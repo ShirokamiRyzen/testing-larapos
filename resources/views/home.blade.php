7a29121634424398ca89576362f948bc30b8781c
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-08-15 10:56:44 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,167 @@
+@extends('layouts.app')
+
+@section('content-header', 'Dashboard')
+
+@section('content')
+<div class="row">
+  <div class="col-md-12">
+  <div class="card card-primary">
+  <div class="card-header info">
+  <h3 class="card-title">Dashboard</h3>
+</div>
+</div>
+</div>
+</div>
+
+<div class="container-fluid">
+    <div class="card-body">
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+            </div>
+        @endif
+
+      {{ __('Kamu login sebagai') }} <b> {{ Auth::user()->name }} </b> <b>
+    @if(Auth::user()->role === "1")
+        (Admin)
+    @elseif(Auth::user()->role === "2")
+        (Kasir)
+    @elseif(Auth::user()->role === "3")
+        (Menunggu Persetujuan)
+    @else
+        (Peran Tidak Dikenal)
+    @endif</b>
+    </div>
+
+    @if (Auth::user()->role == 1 )
+    <div class="row">
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-info">
+          <div class="inner">
+              <h4>{{$orders_count}}</h4>
+            <p>Total Transaksi</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-bag"></i>
+          </div>
+          <div class="small-box">
+          <a href="#" class="small-box-footer">Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+      </div>
+      <!-- ./col -->
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-success">
+          <div class="inner">
+              <h4>{{config('settings.currency_symbol')}} {{number_format($income, 2)}}</h4>
+            <p>Pemasukan</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-stats-bars"></i>
+          </div>
+          <a href="#" class="small-box-footer">Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <!-- ./col -->
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-danger">
+          <div class="inner">
+            <h4>{{$products_count}}</h4>
+            <p>Total Produk</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-pricetags"></i>
+          </div>
+          <a href="/products" class="small-box-footer">Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-warning">
+          <div class="inner">
+              <h4>{{config('settings.currency_symbol')}} {{number_format($outcome, 2)}}</h4>
+            <p>Outcome</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-stats-bars"></i>
+          </div>
+          <a href="#" class="small-box-footer">Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <!-- ./col -->
+</div>
+@endif
+
+@if (Auth::user()->role == 2 )
+    <div class="row">
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-info">
+          <div class="inner">
+              <h4>{{$orders_count}}</h4>
+            <p>Total Transaksi</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-bag"></i>
+          </div>
+          <div class="small-box">
+          <a href="#" class="small-box-footer">Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+      </div>
+      <!-- ./col -->
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-success">
+          <div class="inner">
+              <h4>{{config('settings.currency_symbol')}} {{number_format($income, 2)}}</h4>
+            <p>Pemasukan</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-stats-bars"></i>
+          </div>
+          <a href="#" class="small-box-footer">Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <!-- ./col -->
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-danger">
+          <div class="inner">
+            <h4>{{$products_count}}</h4>
+            <p>Total Produk</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-pricetags"></i>
+          </div>
+          <a href="/products" class="small-box-footer">Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-warning">
+          <div class="inner">
+              <h4>{{config('settings.currency_symbol')}} {{number_format($outcome, 2)}}</h4>
+            <p>Outcome</p>
+          </div>
+          <div class="icon">
+            <i class="ion ion-stats-bars"></i>
+          </div>
+          <a href="#" class="small-box-footer">Selengkapnya <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <!-- ./col -->
+</div>
+@endif
+
+@if (Auth::user()->role == 3 )
+  <div class="alert alert-warning" role="alert">
+    Akunmu sedang menunggu persetujuan Admin
+  </div>
+@endif
+
+</div>
+@endsection
---
+++ resources/views/orders/index.blade.php
@@ -0,0 +1,109 @@
+@extends('layouts.app')
+
+@section('title', 'Orders List')
+@section('content-header', 'Sell Order List')
+@section('content-actions')
+    <!-- <a href="{{route('cart.index')}}" class="btn btn-primary">Open POS</a> -->
+    <ol class="breadcrumb float-sm-right">
+<li class="breadcrumb-item"><a href="#">Home</a></li>
+<li class="breadcrumb-item active">Sell Order</li>
+</ol>
+@endsection
+
+@section('content')
+<div class="card card-primary card-outline">
+    <div class="card-header bg-white">
+        <div class="d-flex justify-content-between">
+        <div>
+        <form action="{{route('orders.index')}}">
+            <div class="d-flex" style="gap: 10px;">
+                <div>
+                    <input type="date" name="start_date" class="form-control" value="{{request('start_date')}}" />
+                </div>
+                <div>
+                    <input type="date" name="end_date" class="form-control" value="{{request('end_date')}}" />
+                </div>
+                <div>
+                    <button class="btn btn-outline-primary" type="submit">Cari</button>
+                </div>
+            </div>
+        </form>
+        </div>
+        <div>
+        <a href="{{route('cart.index')}}" class="btn btn-primary">Open POS</a>
+        </div>
+        </div>
+    </div>
+    <div class="card-body">
+        <table class="table table-datatable">
+        <thead style="background: #F4F6F9">
+                <tr>
+                    <th>No Invoice</th>
+                    <th>Kasir</th>
+                    <th>Total</th>
+                    <th>Received Amount</th>
+                    <th>Status</th>
+                    <th>To Pay</th>
+                    <th>Created At</th>
+					<th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($orders as $order)
+                <tr>
+                    <td>{{
+                        'SO-' . $order->created_at->format('Y') . '/' . $order->created_at->format('dm') . '/' . str_pad($order->id, 4, '0', STR_PAD_LEFT)
+                    }}</td>
+                    <td></td>
+                    <td>{{ config('settings.currency_symbol') }} {{$order->formattedTotal()}}</td>
+                    <td>{{ config('settings.currency_symbol') }} {{$order->formattedReceivedAmount()}}</td>
+                    <td>
+                        @if($order->receivedAmount() == 0)
+                            <span class="badge badge-danger">Not Paid</span>
+                        @elseif($order->receivedAmount() < $order->total())
+                            <span class="badge badge-warning">Partial</span>
+                        @elseif($order->receivedAmount() == $order->total())
+                            <span class="badge badge-success">Paid</span>
+                        @elseif($order->receivedAmount() > $order->total())
+                            <span class="badge badge-info">Change</span>
+                        @endif
+                    </td>
+                    <td>{{config('settings.currency_symbol')}} {{number_format($order->total() - $order->receivedAmount(), 2)}}</td>
+                    <td>{{
+                        // format to 10 Juni 2022
+                        $order->created_at->isoFormat('D MMMM Y')
+                    }}</td>
+					<td>
+					<a href="{{ route('orders.show', $order) }}" class="btn btn-primary"><i class="fas fa-eye"></i></a>
+					<a href="#" class="btn btn-warning">
+                        <i class="fas fa-print"></i>
+                    </a>
+					</td>
+                </tr>
+                @endforeach
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th>{{ config('settings.currency_symbol') }} {{ number_format($total, 2) }}</th>
+                    <th>{{ config('settings.currency_symbol') }} {{ number_format($receivedAmount, 2) }}</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </tfoot>
+        </table>
+    </div>
+</div>
+@endsection
+
+@section('js')
+<script>
+    var table = $('.table-datatable').DataTable({
+        "order": [[ 6, "desc" ]]
+    });
+</script>
+@endsection
+
